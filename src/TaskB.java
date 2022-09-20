b80a09bf0d7a4a22d7ad1f05124eaf9b95c46132
Clément Nguyen <cl__nguyen@eduroam-106160.grenet.fr>
2022-09-20 17:07:29 +0200

    Channel Specification

--- src/TaskB.java
+++ src/TaskB.java
@@ -13,6 +13,8 @@ public class TaskB extends Task {
 	public void run() {
 		m_channel = m_broker.connect("Test", 5555);
 		byte[] bytes = new byte[1024];
+		String s = new String("Hello World");
+		bytes = s.getBytes();
 		m_channel.write(bytes, 0, bytes.length);
 		m_channel.disconnect();
 	}
